31e4a3dd7c4909f7e8b89a6de67da82e774e5c6c
texasaggie97 <aet7yYhk7kgFqNn3>
2015-09-06 20:00:53 -0500

    Fix crash when rotating book detail

--- alexandria/app/src/main/java/com/markesilva/alexandria/BookDetail.java
+++ alexandria/app/src/main/java/com/markesilva/alexandria/BookDetail.java
@@ -30,7 +30,8 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
     private View rootView;
     private String ean;
     private String bookTitle;
-    private ShareActionProvider shareActionProvider;
+    private ShareActionProvider mShareActionProvider;
+    private Intent mShareIntent;
 
     public BookDetail(){
     }
@@ -71,7 +72,8 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         inflater.inflate(R.menu.book_detail, menu);
 
         MenuItem menuItem = menu.findItem(R.id.action_share);
-        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setShareIntent();
     }
 
     @Override
@@ -95,11 +97,11 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         bookTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.TITLE));
         ((TextView) rootView.findViewById(R.id.fullBookTitle)).setText(bookTitle);
 
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
-        shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text)+bookTitle);
-        shareActionProvider.setShareIntent(shareIntent);
+        mShareIntent = new Intent(Intent.ACTION_SEND);
+        mShareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        mShareIntent.setType("text/plain");
+        mShareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text) + bookTitle);
+        setShareIntent();
 
         String bookSubTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.SUBTITLE));
         ((TextView) rootView.findViewById(R.id.fullBookSubTitle)).setText(bookSubTitle);
@@ -124,6 +126,12 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
             rootView.findViewById(R.id.backButton).setVisibility(View.INVISIBLE);
         }
 
+    }
+
+    private void setShareIntent() {
+        if ((mShareActionProvider != null) && (mShareIntent != null)) {
+            mShareActionProvider.setShareIntent(mShareIntent);
+        }
     }
 
     @Override
